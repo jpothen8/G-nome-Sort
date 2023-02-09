f9e4bbf0b86abbf5fc42d03f5c863f7eba66785c
jpothen8 <jpothen8@gmail.com>
2023-02-09 11:08:55 -0500

    Test scenario 1

--- GnomeSort.kt
+++ GnomeSort.kt
@@ -1,14 +1,20 @@
 class GnomeSort {
-    fun sort(arr: IntArray) {
+    fun sort(arr: IntArray): Long {
         var index = 0
+        var operations: Long = 0
         while (index < arr.size) {
             if (index == 0) index++
-            if (arr[index] >= arr[index - 1]) index++ else {
+            if (arr[index] >= arr[index - 1]) {
+                operations++
+                index++
+            } else {
+                operations += 3
                 val temp = arr[index]
                 arr[index] = arr[index - 1]
                 arr[index - 1] = temp
                 index--
             }
         }
+        return operations
     }
 }
--- Main.kt
+++ Main.kt
@@ -1,25 +1,28 @@
 @file:JvmName("Main")
 
 import kotlin.random.Random
+import kotlin.system.measureNanoTime
+
 fun main() {
         var test = 1
         
-        var totalElapsed = 0
+        var totalElapsed: Long = 0
+
+        var operations: Long = 0
         
         while(test <= 50) {
                 val testArray = IntArray(10000) { Random.nextInt(1, 1001) }
 
-                println("Original List: " + testArray.contentToString())
-
-                val elapsed = measureNanoTime { GnomeSort().sort(testArray) }
-
-                totalElapsed += elapsed
+                totalElapsed += measureNanoTime { operations += GnomeSort().sort(testArray) }
                 
                 test++
         }
         
-        print("Average time in nanoseconds to run Gnome Sort with randomized array: ")
+        print("Average time in nanoseconds to run Gnome Sort with a randomized array: ")
         println(totalElapsed / 50)
+
+        print("Average operations ran with Gnome Sort with a randomized array: ")
+        println(operations / 50)
         
         println("                                                                   \n" +
                 " ,adPPYb,d8 8b,dPPYba,   ,adPPYba,  88,dPYba,,adPYba,   ,adPPYba,  \n" +
